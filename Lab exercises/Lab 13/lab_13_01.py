e93c0e2439f53d1505c06819965929d1ad80306a
Muhammad-Ahmed-Qazi <qmuhammadahmed@gmail.com>
2024-11-19 10:35:38 +0500

    Some revision lab work.

--- Lab exercises/Lab 13/lab_13_01.py
+++ Lab exercises/Lab 13/lab_13_01.py
@@ -1,3 +1,32 @@
-with open("data.txt", "r") as file:
-    for line in file:
-        print(line, end="")
+f=open()
+
+
+
+print('hello')
+print('add as much data as you want then press q to exit')
+while True:
+    data={}
+    n=[]
+    r=[]
+    name = input('enter name of students: ')
+    roll_no =input('enter roll no of student:')
+    if roll_no in r:
+        print (f'{roll_no} already exist')
+    n.append(name)
+    r.append(roll_no)
+
+    if roll_no=='':
+        break
+
+for i in r:
+    for j in n:
+        print(i)
+
+
+directory
+import os
+data=os.join.path()
+
+
+
+
